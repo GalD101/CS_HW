d98dd010b642da9d1511887ec841f3cbfd63d2cb
GalD101 <galdali10@gmail.com>
2022-12-03 21:47:49 +0200

    i think im done but jusr small improvements left

--- ex_4.c
+++ ex_4.c
@@ -14,46 +14,49 @@
 * The output: *
 * The Function operation: initialize a 2d char array with an initial value *
 *************************************************************************/
-void initSection(char section[][SECTION_SIZE], int size, char initVal) {
+void initBoard(char board[][SECTION_SIZE][SECTION_SIZE], int size, char
+initVal) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            section[i][j] = initVal;
+            for (int k = 0; k < size * (i + 1); k++) {
+                board[i][j][k] = initVal;
+            }
         }
     }
 }
 
-void printRow(char row[], int size) {
-    for (int i = 0; i < SECTION_SIZE; i++) {
-        printf("%c ", row[i]);
-    }
-    printf("\n");
-}
+//void printRow(char row[], int size) {
+//    for (int i = 0; i < size; i++) {
+//        printf("%c ", row[i]);
+//    }
+//    printf("\n");
+//}
 
-void printSection(int sectionIndex, char section[], int size) {
+void printBoard(char section[][SECTION_SIZE][SECTION_SIZE], int size) {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            setbuf(stdout, 0);
-            printf("(%d %d %d) ", sectionIndex, i, j);
-        }
-        for (int k = i*size; k < size*(i+1); k++) {
-            setbuf(stdout, 0);
-            printf("%c ", section[k]);
+            for (int k = 0; k < size; ++k) {
+                printf("(%d %d %d) ", i, j, k);
+            }
+            for (int k = 0; k < size; ++k) {
+                printf("%c ", section[i][j][k]);
+            }
+            printf("\n");
         }
         printf("\n");
     }
 }
 
-// return the char of the winner. return '0' if tie (only possible output is tie, X win or O win)
+// return the char of the winner. return '_' if tie (only possible output is tie, X win or O win)
 char determineRowWinner(char section[][SECTION_SIZE], int size) {
-    char winner = '0';
+    char winner = 'z';
     for (int i = 0; i < size; ++i) {
         char prev = section[i][0];
         for (int j = 1; j < size; ++j) {
             char cur = section[i][j];
             if (cur != prev) {
-                return '0';
-            }
-            else {
+                return '_';
+            } else {
                 winner = cur;
             }
             prev = cur;
@@ -62,17 +65,16 @@ char determineRowWinner(char section[][SECTION_SIZE], int size) {
     return winner;
 }
 
-// return the char of the winner. return '0' if tie (only possible output is tie, X win or O win)
+// return the char of the winner. return '_' if tie (only possible output is tie, X win or O win)
 char determineColWinner(char section[][SECTION_SIZE], int size) {
-    char winner = '0';
+    char winner = 'z';
     for (int i = 0; i < size; ++i) {
         char prev = section[0][i];
         for (int j = 1; j < size; ++j) {
             char cur = section[j][i];
             if (cur != prev) {
-                return '0';
-            }
-            else {
+                return '_';
+            } else {
                 winner = cur;
             }
             prev = cur;
@@ -82,16 +84,15 @@ char determineColWinner(char section[][SECTION_SIZE], int size) {
 }
 
 
-// return the char of the winner. return '0' if tie (only possible output is tie, X win or O win)
+// return the char of the winner. return '_' if tie (only possible output is tie, X win or O win)
 char determineDiagWinner(char section[][SECTION_SIZE], int size) {
-    char winner = '0';
+    char winner = 'z';
     for (int i = 1; i < size; ++i) {
         char prev = section[i - 1][i - 1];
         char cur = section[i][i];
         if (cur != prev) {
-            return '0';
-        }
-        else {
+            return '_';
+        } else {
             winner = cur;
         }
     }
@@ -100,12 +101,48 @@ char determineDiagWinner(char section[][SECTION_SIZE], int size) {
 }
 
 
+int determineWinnerBoard(int size, char board[][SECTION_SIZE][SECTION_SIZE],
+                         int isGameOver) {
+    const char TIE = '_';
+    const char NO_WINNER = 'z';
+
+    for (int i = 0; i < size; ++i) {
+        char rowWinner = determineRowWinner(board[i], size);
+        if (rowWinner == 'X' || rowWinner == 'O') {
+            return rowWinner;
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        char colWinner = determineColWinner(board[i], size);
+        if (colWinner == 'X' || colWinner == 'O') {
+            return colWinner;
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        char diagWinner = determineDiagWinner(board[i], size);
+        if (diagWinner == 'X' || diagWinner == 'O') {
+            return diagWinner;
+        }
+    }
+
+    if (board[0][0][0] == board[1][1][1] && board[0][0][0] == board[2][2][2] &&
+        board[0][0][0] == board[3][3][3]) {
+        if (board[0][0][0] == 'X' || board[0][0][0] == 'O') {
+            return board[0][0][0];
+        }
+    }
+    return isGameOver ? TIE : NO_WINNER;
+}
+
+
 int determineWinner(int size,
                     char section0[][SECTION_SIZE],
                     char section1[][SECTION_SIZE],
                     char section2[][SECTION_SIZE],
                     char section3[][SECTION_SIZE]) {
-    const char TIE = '0';
+    const char TIE = '_';
 
     char rowWinner0 = determineRowWinner(section0, size);
     char rowWinner1 = determineRowWinner(section1, size);
@@ -122,7 +159,8 @@ int determineWinner(int size,
     char diagWinner2 = determineDiagWinner(section2, size);
     char diagWinner3 = determineDiagWinner(section3, size);
 
-    if (section0[0][0] == section1[1][1] && section0[0][0] == section2[2][2] && section0[0][0] == section3[3][3]) {
+    if (section0[0][0] == section1[1][1] && section0[0][0] == section2[2][2] &&
+        section0[0][0] == section3[3][3]) {
         if (section0[0][0] == 'X' || section0[0][0] == 'O') {
             return section0[0][0];
         }
@@ -242,10 +280,21 @@ int isValidInput(char input) {
 
 
 // todo: check how to define a char like '*' in a preprocessor
-int updateSection(char section[][SECTION_SIZE], int row, int col, int isXturn, const char EMPTY_SPOT) {
+int updateSection(char section[][SECTION_SIZE], int row, int col, int isXturn,
+                  const char EMPTY_SPOT) {
     int isSpotEmpty = section[row][col] == EMPTY_SPOT;
     if (isSpotEmpty) {
-        section[row][col] = isXturn ? 'X':'O';
+        section[row][col] = isXturn ? 'X' : 'O';
+    }
+
+    return !isSpotEmpty;
+}
+
+int updateBoard(char board[][SECTION_SIZE][SECTION_SIZE],int section,
+                int row, int col, int isXturn, const char EMPTY_SPOT) {
+    int isSpotEmpty = board[section][row][col] == EMPTY_SPOT;
+    if (isSpotEmpty) {
+        board[section][row][col] = isXturn ? 'X' : 'O';
     }
 
     return !isSpotEmpty;
@@ -254,21 +303,15 @@ int updateSection(char section[][SECTION_SIZE], int row, int col, int isXturn, c
 // game() will return 0 if there is a winner or a tie. It will return a 1 if there was an error
 int game() {
     const char EMPTY_SPOT = '*';
-    char winner = '_';
-    char section0 [SECTION_SIZE][SECTION_SIZE];
-    char section1 [SECTION_SIZE][SECTION_SIZE];
-    char section2 [SECTION_SIZE][SECTION_SIZE];
-    char section3 [SECTION_SIZE][SECTION_SIZE];
+    char winner = 'z';
+    char board[SECTION_SIZE][SECTION_SIZE][SECTION_SIZE];
     enum pos {
         section = 0,
         row = 1,
         column = 2,
     };
 
-    initSection(section0, SECTION_SIZE, EMPTY_SPOT);
-    initSection(section1, SECTION_SIZE, EMPTY_SPOT);
-    initSection(section2, SECTION_SIZE, EMPTY_SPOT);
-    initSection(section3, SECTION_SIZE, EMPTY_SPOT);
+    initBoard(board, SECTION_SIZE, EMPTY_SPOT);
 
     char input = getchar();
     int index = 0;
@@ -276,9 +319,9 @@ int game() {
     while (input == '\n' || input == ' ') {
         input = getchar();
     }
-    while (winner == '_') { //!(winner || isTie)
+    while (winner == 'z') { //!(winner || isTie)
         int chosenSection = 0, chosenRow = 0, chosenColumn = 0;
-        while (input != '\n') {
+        while (input != '\n' && winner == 'z') {
             if (!isValidInput(input)) {
                 return 1;
             }
@@ -295,31 +338,14 @@ int game() {
                 case column: {
                     chosenColumn = integerInput;
 
-                    if (chosenSection != -1 && chosenRow != -1 && chosenColumn != -1) {
-                        switch (chosenSection) {
-                            case 0:
-                                if (updateSection(section0, chosenRow, chosenColumn, isXturn, EMPTY_SPOT)) {
-                                    return 1;
-                                }
-                                break;
-                            case 1:
-                                if (updateSection(section1, chosenRow, chosenColumn, isXturn, EMPTY_SPOT)) {
-                                    return 1;
-                                }
-                                break;
-                            case 2:
-                                if (updateSection(section2, chosenRow, chosenColumn, isXturn, EMPTY_SPOT)) {
-                                    return 1;
-                                }
-                                break;
-                            case 3:
-                                if (updateSection(section3, chosenRow, chosenColumn, isXturn, EMPTY_SPOT)) {
-                                    return 1;
-                                }
-                                break;
-                            default:
-                                break;
+                    if (chosenSection != -1 && chosenRow != -1 &&
+                        chosenColumn != -1) {
+                        if (updateBoard(board, chosenSection,
+                                        chosenRow, chosenColumn, isXturn,
+                                        EMPTY_SPOT)) {
+                            return 1;
                         }
+                        winner = determineWinnerBoard(SECTION_SIZE, board, 0);
                         isXturn = !isXturn;
                     }
                     break;
@@ -343,7 +369,9 @@ int game() {
         if (index < 2) {
             return 1;
         }
-        winner = determineWinner(SECTION_SIZE, section0, section1, section2, section3);
+        if (winner == 'z') {
+            winner = determineWinnerBoard(SECTION_SIZE, board, 1);
+        }
         switch (winner) {
             case 'X':
                 printf("X is the winner.\n");
@@ -351,31 +379,26 @@ int game() {
             case 'O':
                 printf("O is the winner.\n");
                 break;
-            case '0':
-                printf("Tie.\n");
-                break;
+            case '_':
             default:
                 printf("Tie.\n");
                 break;
         }
 
-        printSection(0, section0, SECTION_SIZE);
-        printf("\n");
-        printSection(1, section1, SECTION_SIZE);
-        printf("\n");
-        printSection(2, section2, SECTION_SIZE);
-        printf("\n");
-        printSection(3, section3, SECTION_SIZE);
-        printf("\n");
+        printBoard(board, SECTION_SIZE);
     }
 
     return 0;
 }
 
 
-int main () {
+int main() {
+//    char a [SECTION_SIZE][SECTION_SIZE][SECTION_SIZE];
+//
+//    initBoard(a, SECTION_SIZE, '*');
+//    printBoard(a, SECTION_SIZE);
     printf("Would you like to start? (y/n)\n");
-    while(1) {
+    while (1) {
         char choice;
         scanf("%c", &choice);
         switch (choice) {
